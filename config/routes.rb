c63c59e0c2ca800ac24e1038b9fc0bb5c352f3bc
Sara OC <saramoo@hotmail.com>
2017-11-10 13:40:15 +0000

    install semantic-ui

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :users, only: [:create] do
     resource :password,
       controller: "clearance/passwords",
-      only: [:create, :edit, :update]
+      only:       [:create, :edit, :update]
   end
 
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
